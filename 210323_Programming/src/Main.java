b0b817f81dd2ae031cf4ad1835e0122c7e245dff
Seongho Moon <now_start2691@naver.com>
2021-03-23 10:00:27 +0900

    commit

---
+++ 210323_Programming/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int edge = sc.nextInt();
+		int[][] node = new int[n + 1][n + 1];
+
+		for (int i = 0; i < edge; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			node[y][x] = node[x][y] = 1;
+		}
+
+		int count = DFS(node);
+
+		System.out.println(count);
+	}
+
+	private static int DFS(int[][] node) {
+		int count = 0;
+		int[] visit = new int[node.length];
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.add(1);
+		visit[1] = 1;
+
+		while (!queue.isEmpty()) {
+			int i = queue.poll();
+			for (int j = 1; j < node.length; j++) {
+				if (node[i][j] == 1 && visit[j] == 0) {
+					queue.add(j);
+					visit[j] = 1;
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+}
