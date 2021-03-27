63f5d559bd5510fcf6eb67ffa0dff84daffca8c0
Seongho Moon <now_start2691@naver.com>
2021-03-27 22:12:46 +0900

    commit

--- 210329_Programming/src/Main.java
+++ 210329_Programming/src/Main.java
@@ -1,8 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		String p = "I";
+		String s = br.readLine();
+
+		for (int i = 0; i < n; i++) {
+			p += "OI";
+		}
+
+		for (int i = 0; i < m; i++) {
+			for(int j=i;j<n;j++) {
+				
+			}
+		}
 
 	}
 
