151dbe3382407f7892153278c9bb2a9df032cc31
Seongho Moon <now_start2691@naver.com>
2021-04-11 17:40:35 +0900

    commit

--- 210418_Programming/src/Main.java
+++ 210418_Programming/src/Main.java
@@ -1,9 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n + 1];
 
+		st = new StringTokenizer(br.readLine());
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+			sum += Integer.parseInt(st.nextToken());
+			arr[i] = sum;
+		}
+
+		while (m-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken()) - 1;
+			int end = Integer.parseInt(st.nextToken());
+
+			bw.write(arr[end] - arr[start] + "\n");
+		}
+
+		bw.flush();
 	}
 
 }
