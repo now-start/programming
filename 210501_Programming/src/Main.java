94a90c984fe6e0f6ef12cf44f2f394023418e656
Seongho Moon <now_start2691@naver.com>
2021-05-16 22:51:18 +0900

    commit

--- 210501_Programming/src/Main.java
+++ 210501_Programming/src/Main.java
@@ -1,9 +1,40 @@
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		Stack<Character> stack = new Stack<>();
 
+		// main
+		for (int i = 0; i < s.length(); i++) {
+			char temp = s.charAt(i);
+
+			if (temp == '(') {
+				stack.push(temp);
+			} else if (temp == ')') {
+				while (!stack.isEmpty() && stack.peek() != '(') {
+					System.out.print(stack.pop());
+				}
+				stack.pop();
+			} else if (temp == '+' || temp == '-') {
+				while (!stack.isEmpty() && stack.peek() != '(') {
+					System.out.print(stack.pop());
+				}
+				stack.add(temp);
+			} else if (temp == '*' || temp == '/') {
+				while (!stack.isEmpty() && (stack.peek() == '*' || stack.peek() == '/')) {
+					System.out.print(stack.pop());
+				}
+				stack.add(temp);
+			} else {
+				System.out.print(temp);
+			}
+		}
+
+		while (!stack.isEmpty()) {
+			System.out.print(stack.pop());
+		}
 	}
-
 }
