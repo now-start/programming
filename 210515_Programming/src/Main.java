26363d72dfe9997c14d5542c411fbf0174590cbf
Seongho Moon <now_start2691@naver.com>
2021-05-17 23:32:39 +0900

    commit

--- 210515_Programming/src/Main.java
+++ 210515_Programming/src/Main.java
@@ -1,9 +1,24 @@
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// nCm = n!/m!(n-m)!
+		// 100!/6!(100-6)! = 100!/6!94! = 100 * ... * 95 / 6 * ... * 1
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
 
+		BigInteger temp1 = BigInteger.ONE;
+		BigInteger temp2 = BigInteger.ONE;
+
+		for (int i = 0; i < m; i++) {
+			temp1 = temp1.multiply(BigInteger.valueOf(n - i));
+			temp2 = temp2.multiply(BigInteger.valueOf(i + 1));
+		}
+
+		System.out.println(temp1.divide(temp2));
 	}
 
 }
