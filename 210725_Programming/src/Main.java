44471f23268cb35dcb666699854e16a2aa5288f6
Seongho Moon <now_start2691@naver.com>
2021-07-14 00:47:03 +0900

    commit

--- 210725_Programming/src/Main.java
+++ 210725_Programming/src/Main.java
@@ -3,8 +3,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-//1013
+//1946
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -13,7 +15,33 @@ public class Main {
 		int t = Integer.parseInt(br.readLine());
 
 		while (t-- > 0) {
+			int n = Integer.parseInt(br.readLine());
+			int[][] score = new int[n][2];
 
+			for (int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				score[i][0] = Integer.parseInt(st.nextToken());
+				score[i][1] = Integer.parseInt(st.nextToken());
+			}
+
+			Arrays.sort(score, (int[] o1, int[] o2) -> {
+				if (o1[0] > o2[0])
+					return 1;
+				else
+					return -1;
+			});
+
+			// main
+			int count = 1;
+			int temp = score[0][1];
+			for (int i = 1; i < n; i++) {
+				if (temp > score[i][1]) {
+					count++;
+					temp = score[i][1];
+				}
+			}
+
+			bw.write(count + "\n");
 		}
 
 		bw.flush();
