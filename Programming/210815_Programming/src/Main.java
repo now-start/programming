c850b0b4ee6bd34d3d981858a9f497e8d800a3ec
Seongho Moon <now_start2691@naver.com>
2021-08-20 20:11:09 +0900

    210815 commit

--- Programming/210815_Programming/src/Main.java
+++ Programming/210815_Programming/src/Main.java
@@ -2,10 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
-		System.out.println("git test");
-		System.out.println("last git test");
+		
 	}
 
 }
