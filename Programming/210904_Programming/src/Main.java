296764ba55339c63753205af4672b0e5ddef6571
Seongho Moon <now_start2691@naver.com>
2021-09-11 23:15:59 +0900

    15649_commit

--- Programming/210904_Programming/src/Main.java
+++ Programming/210904_Programming/src/Main.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 
+// 15649
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
 
+		int[] arr = new int[m];
+		boolean[] visit = new boolean[n];
+
+		DFS(arr, visit, n, 0, 0);
+
+	}
+
+	public static void DFS(int[] arr, boolean[] visit, int n, int index, int now) {
+		if (index == arr.length) {
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			if (!visit[i-1]) {
+				visit[i-1] = true;
+				arr[index] = i;
+				DFS(arr, visit, n, index + 1, i);
+				visit[i-1] = false;
+			}
+		}
 	}
 
 }
