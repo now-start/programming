8dbba8c1002662ed6b76e250aedd352f4cfd9477
SeongHo-Moon <nowstart1468@medialog.co.kr>
2021-10-20 15:47:05 +0900

    2947_commit

---
+++ Programming/211024_Programming/src/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 2947
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// input
+		String s = br.readLine();
+		String[] temp = s.split(" ");
+		int[] arr = new int[temp.length];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(temp[i]);
+		}
+
+		// main
+		for (int t = 0; t < arr.length; t++) {
+			for (int i = 0; i < arr.length - 1; i++) {
+				if (arr[i] > arr[i + 1]) {
+					int tmp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = tmp;
+
+					// output
+					for (int j = 0; j < arr.length; j++) {
+						bw.write(arr[j] + " ");
+					}
+					bw.write("\n");
+				}
+			}
+		}
+
+		bw.flush();
+	}
+
+}
