a7e747b0e680da82622604900b7a41e12f9d63bd
SeongHo-Moon <nowstart1468@medialog.co.kr>
2021-10-28 16:05:54 +0900

    1158_commit

---
+++ Programming/211028_Programming/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// 1158
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		// 삭제가 계속 일어나기 때문에 ArrayList 보다 LinkedList 속도가 더 빠름
+		List<Integer> list = new LinkedList<Integer>();
+		for (int i = 1; i <= n; i++) {
+			list.add(i);
+		}
+
+		bw.write("<");
+		while (list.size() != 1) {
+			for (int i = 1; i < k; i++) {
+				int temp = list.remove(0);
+				list.add(temp);
+			}
+			bw.write(list.remove(0) + ", ");
+		}
+		bw.write(list.remove(0) + "");
+		bw.write(">");
+
+		bw.flush();
+	}
+
+}
