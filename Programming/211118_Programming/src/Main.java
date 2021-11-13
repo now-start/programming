c0e7cc2d11fe35fbe635bbaf2697db6739a94752
Seongho Moon <now_start2691@naver.com>
2021-11-14 00:16:05 +0900

    1934_commit

---
+++ Programming/211118_Programming/src/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+// 1934
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int t = Integer.parseInt(br.readLine());
+
+		while (t-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int gcd; // 최대공약수
+			int lcm; // 최소공배수
+
+			// 유클리우드 호제법
+			if (a >= b) {
+				gcd = getGcd(a, b);
+			} else {
+				gcd = getGcd(b, a);
+			}
+			lcm = a * b / gcd;
+			bw.write(lcm+"\n");
+		}
+
+		bw.flush();
+	}
+
+	public static int getGcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		}
+		return getGcd(b, a % b);
+	}
+
+}
