34cfc95521b8936764c85003d77a9c7e8950f719
SeongHo-Moon <nowstart1468@medialog.co.kr>
2021-11-25 14:23:04 +0900

    3055_init

---
+++ Programming/211202_Programming/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 3055
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
