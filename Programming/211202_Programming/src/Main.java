145ff423031e0db9671c853d2783315fff096ddf
Seongho Moon <now_start2691@naver.com>
2021-11-26 23:45:27 +0900

    5523_commit

--- Programming/211202_Programming/src/Main.java
+++ Programming/211202_Programming/src/Main.java
@@ -1,16 +1,34 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-// 3055
+// 5523
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int n = Integer.parseInt(br.readLine());
 
+		int countA = 0;
+		int countB = 0;
+
+		while (n-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (a > b) {
+				countA++;
+			} else if (a < b) {
+				countB++;
+			}
+		}
+
+		bw.write(countA + " " + countB);
+		bw.flush();
 	}
 
 }
