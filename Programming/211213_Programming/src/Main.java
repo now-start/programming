ea8e9736b13b3e5c60d780b35656a57f0eee5dfe
SeongHo-Moon <nowstart1468@medialog.co.kr>
2021-12-03 11:07:23 +0900

    18096_commit

---
+++ Programming/211213_Programming/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 18096
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+		// 문제 해석
+		// n이 주어졌을 때 ((x+1)(y+1) - xy - x - y)^n을 구하는 문제 (단, x와 y는 알 수 없음)
+		bw.write(1 + " ");
+		bw.flush();
+	}
+
+}
