3e8d5ab43c39e891ef370985b4faca1781558c8f
Moon <nowstart2691@gmail.com>
2022-02-13 18:21:09 +0900

    5338_commit

--- Programming/220213_Programming/src/Main.java
+++ Programming/220213_Programming/src/Main.java
@@ -2,9 +2,8 @@
 //5337
 public class Main {
     public static void main(String[] args) {
-        System.out.println("""
-                .  .   .
-                |  | _ | _. _ ._ _  _
-                |/\\|(/.|(_.(_)[ | )(/.""");
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
     }
 }
