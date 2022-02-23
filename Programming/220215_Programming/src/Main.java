cea47421d2cd96fe49459c747b4e8ab11e980e7f
Moon <nowstart2691@gmail.com>
2022-02-23 14:04:50 +0900

    7287_commit

---
+++ Programming/220215_Programming/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+//7287
+public class Main {
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        bw.write("264\nphantom2691");
+        bw.flush();
+    }
+}
