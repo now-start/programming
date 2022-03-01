5c09c55eaa74ac0d0a7051f2434b29b87c324650
MOON <nowstart2691@gmail.com>
2022-03-01 22:51:55 +0900

    220301_commit

---
+++ Programming/220301_Programming/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+
+    public static void main(String[] args) {
+
+    }
+}
