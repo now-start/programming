8e7b7c6ba56a30aa6a586ac45c445493a643dae2
moon <nowstart2691@lguplus.co.kr>
2023-03-06 15:45:25 +0900

    commit 2022

--- Programming/230306_Programming/src/Main.java
+++ Programming/230306_Programming/src/Main.java
@@ -1,15 +1,36 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 import static java.lang.System.in;
 import static java.lang.System.out;
 
-// 25083
+// 2022
 public class Main {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(in));
     private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
 
     public static void main(String[] args) throws IOException {
-        out.println("args = " + Arrays.deepToString(args));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double x = Double.parseDouble(st.nextToken());
+        double y = Double.parseDouble(st.nextToken());
+        double c = Double.parseDouble(st.nextToken());
+
+
+        double left = 0;
+        double right = Math.min(x, y);
+
+        while (right - left >= 0.001) {
+            double width = (left + right) / 2;
+            double h1 = Math.sqrt(Math.pow(x, 2) - Math.pow(width, 2));
+            double h2 = Math.sqrt(Math.pow(y, 2) - Math.pow(width, 2));
+            double res = (h1 * h2) / (h1 + h2);
+
+            if (res >= c) left = width;
+            else right = width;
+        }
+
+        bw.write(String.format("%.3f", right));
+        bw.flush();
     }
 }
