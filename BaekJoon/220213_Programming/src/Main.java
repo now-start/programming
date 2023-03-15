d9848a96ae7fbcdd021c09e6f32b7b1407cb4439
moon <nowstart2691@gmail.com>
2023-03-15 09:59:04 +0900

    modify directory

--- BaekJoon/220213_Programming/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-//5337
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(".  .   .\n" +
-                "|  | _ | _. _ ._ _  _\n" +
-                "|/\\|(/.|(_.(_)[ | )(/.");
-    }
-}
